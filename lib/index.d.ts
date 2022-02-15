a9e31e83e36fc4c5d54535a543f76f52ee92ff6d
chlbri <bri_lvi@icloud.com>
2022-02-15 18:05:42 +0000

    ok

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,13 @@
 export * from './clean';
+export * from './isUpToDate';
 export * from './pack';
 export * from './rinit';
-export * from './isUpToDate';
+export * from './versionnize';
+import { VersionProps } from './versionnize';
 declare type Props = {
     currentBranch?: string;
     productionBranch?: string;
     lib?: string;
+    versionProps?: VersionProps;
 };
-export default function publish({ currentBranch, productionBranch, lib, }: Props): void;
+export default function publish({ currentBranch, productionBranch, lib, versionProps, }: Props): Promise<void>;
--- lib/pack.d.ts
+++ lib/pack.d.ts
@@ -1,1 +1,7 @@
-export declare function pack(lib: string): void;
+import { VersionProps } from './versionnize';
+declare type Props = {
+    lib: string;
+    versionProps: VersionProps;
+};
+export declare function pack({ lib, versionProps }: Props): void;
+export {};
